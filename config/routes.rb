349c9e59da65f5e6e18110d807f2f173a469c462
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-10-03 10:58:48 +0000

    users/show.html.erb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   root to: 'homes#top'
   get 'about' => 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
