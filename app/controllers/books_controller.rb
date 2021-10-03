11829ffd45646b534597f26f62336e4ae8db99d3
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-10-03 12:54:16 +0000

    部分テンプレート実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,14 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path
+      redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -19,6 +20,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def destroy
